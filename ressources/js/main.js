3a796cf721fa3b355356fe5cb23094265b71296f
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-03-26 14:35:34 +0000

    adding isotope js

---
+++ ressources/js/main.js
@@ -0,0 +1,21 @@
+//***********isotope js
+var $portfolioItem = $('.portfolio-item');
+console.log($portfolioItem)
+
+$portfolioItem.isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+
+$('.portfolio-menu button').on('click', function(e){
+
+    e.preventDefault();
+
+    var filter = $(this).attr('data-filter');
+
+    $('.portfolio-menu button').removeClass('active');
+    $(this).addClass('active');
+
+    $portfolioItem.isotope({filter: filter});
+
+});
