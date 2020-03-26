8fe0251e98a41ec544680802863e40bce2e6f2ac
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-03-26 23:03:55 +0000

    clipPath

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,21 +1,42 @@
-//***********isotope js
-var $portfolioItem = $('.portfolio-item');
-// console.log($portfolioItem)
 
-$portfolioItem.isotope({
-    itemSelector: '.item',
-    layoutMode: 'fitRows'
-});
+$(document).ready(function () {
+        /*<-scorll navbar js->*/
+    $(window).scroll(function(){
+        // console.log('yes')
+        if($(window).scrollTop() > 60 ){
+            // console.log($(window).scrollTop())
+            $('.navbar').addClass('d-none')
+        }
+        if($(window).scrollTop() < 61 ){
+            $('.navbar').removeClass('d-none')
+        }
+        if($(window).scrollTop() > 101){
+            // console.log('yes')
+            $('.carousel-item').addClass('clP')
+        }
+        if($(window).scrollTop() < 102){
+            $('.carousel-item').removeClass('clP')
+        }
+    })
 
-$('.portfolio-menu button').on('click', function(e){
+    //***********isotope js
+    var $portfolioItem = $('.portfolio-item');
+    // console.log($portfolioItem)
 
-    e.preventDefault();
+    $portfolioItem.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
 
-    var filter = $(this).attr('data-filter');
+    $('.portfolio-menu button').on('click', function(e){
 
-    $('.portfolio-menu button').removeClass('active');
-    $(this).addClass('active');
+        e.preventDefault();
 
-    $portfolioItem.isotope({filter: filter});
+        var filter = $(this).attr('data-filter');
+
+        $('.portfolio-menu button').removeClass('active');
+        $(this).addClass('active');
+        $portfolioItem.isotope({filter: filter});
+    });
 
 });
